5ebfe0272f16384a41cb9031790e67c68bae75e6
fverzelloni <fabio.verzelloni@gmail.com>
2014-12-02 11:58:29 +0100

    check nodes down

---
+++ check_nodes_down.sh
@@ -0,0 +1,16 @@
+#~/bin/bash
+
+STATE_OLD=/tmp/current
+STATE_NOW=/tmp/old
+
+result=$(comm -2 $STATE_NOW $STATE_OLD )
+
+if [ $? -eq 0 ]
+then
+        echo "files are the same"
+else
+        echo "files are different"
+        echo "$result"
+fi
+
+echo $result
