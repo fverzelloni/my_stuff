379d7f1f75d197c0e5da98b968240ed43e4334fa
fverzelloni <fverzell@cscs.ch>
2014-12-10 14:34:30 +0100

    update with if -n

--- make_list.sh
+++ make_list.sh
@@ -6,13 +6,18 @@ LOCATION="/apps/$HOSTNAME/regression/out/$LAST/[e..i]$HOSTNAME/$HOSTNAME*"
 NID=$(grep nid $LOCATION | awk '{ print $1 }' | paste -sd ",")
 GET_NODES=$(scontrol show res=syscheck | grep -w Nodes | awk '{ print $1 }' | cut -d "[" -f2 | cut -d "]" -f1)
 
-echo -e  "This is the list of nodes that needs to be added to the syscheck reservation"
-echo -e  "since they have not passed the regression test:"
-echo ""
-echo -e  "\033[31m $NID"
-tput sgr0;
-echo ""
-echo "Please run:"
-echo ""
-echo -e "\033[31m scontrol update res=syscheck nodes=$GET_NODES,$NID"
-tput sgr0;
+if [ ! -n $NID ]
+	then
+		echo -e  "This is the list of nodes that needs to be added to the syscheck reservation"
+		echo -e  "since they have not passed the regression test:"
+		echo ""
+		echo -e  "\033[31m $NID"
+		tput sgr0;
+		echo ""
+		echo "Please run:"
+		echo ""
+		echo -e "\033[31m scontrol update res=syscheck nodes=$GET_NODES,$NID"
+		tput sgr0;
+	else
+		echo "All nodes passed the regression test"
+fi
