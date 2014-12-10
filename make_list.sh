e803da3216eeda75977127301da3964c22daa8ca
fverzelloni <fverzell@cscs.ch>
2014-12-10 15:07:46 +0100

    colors added

--- make_list.sh
+++ make_list.sh
@@ -3,21 +3,23 @@
 HOSTNAME=$(hostname  | sed 's/[0-9]//g')
 LAST=$(ls -latr /apps/$HOSTNAME/regression/out/ | tail -n1 | awk '{ print $9 }')
 LOCATION="/apps/$HOSTNAME/regression/out/$LAST/[e..i]$HOSTNAME/$HOSTNAME*"
-NID=$(grep nid $LOCATION | awk '{ print $1 }' | paste -sd ",")
-GET_NODES=$(scontrol show res=syscheck | grep -w Nodes | awk '{ print $1 }' | cut -d "[" -f2 | cut -d "]" -f1)
+GET_NODES=$(scontrol show res=syscheck | grep -w Nodes | awk '{ print $1 }' )
+PATHTOWIDTH=5
+NID=$(grep nid $LOCATION | awk '{ print $1 }' | paste -sd "," )
 
 if [ ! -z $NID ]
 	then
-		echo -e  "This is the list of nodes that needs to be added to the syscheck reservation"
+		echo -e  "\033[31m \e[1m This is the list of nodes that needs to be added to the syscheck reservation "
 		echo -e  "since they have not passed the regression test:"
-		echo ""
-		echo -e  "\033[31m $NID"
 		tput sgr0;
 		echo ""
-		echo "Please run:"
+		echo -e  "$NID"
 		echo ""
-		echo -e "\033[31m scontrol update res=syscheck nodes=$GET_NODES,$NID"
+		echo -e "\033[31m \e[1m Please run:"
 		tput sgr0;
+		echo ""
+		echo -e "scontrol update res=syscheck $GET_NODES,$NID" 
 	else
-		echo "All nodes passed the regression test"
+		echo -e "\033[33;32m All nodes passed the regression test"
+		tput sgr0;
 fi
