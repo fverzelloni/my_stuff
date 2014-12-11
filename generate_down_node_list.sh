3b82da10f50e19264e272e853e01a5b011e8de32
fverzelloni <fverzell@cscs.ch>
2014-12-11 14:50:05 +0100

    -c option

--- generate_down_node_list.sh
+++ generate_down_node_list.sh
@@ -27,6 +27,13 @@ case "$1" in
 			rm list1 list3
 			;;
 
+	"-c" | "--count" )
+                        xtprocadmin |grep down | tr "," "\n" | awk '{ print $1" "$5" "$3 }' > list1
+                        cat list1 | awk '{ print $1 }' | while read line ; do printf "nid%0*d\n" $pathtowith $line ; done > list3
+                        paste list3 list1 | column -s $'\t' -t | awk '{ print $1","" "","" "","" "","" "","" ""," $3" "","$4 }' | wc -l
+                        rm list1 list3
+                        ;;
+
 	"-h" | "--help" )
 			echo "usage: ${0##*/} -f|--file FILENAME {to create a file} "
 			echo "usage: ${0##*/} -o|--output {to print in stdout}"
