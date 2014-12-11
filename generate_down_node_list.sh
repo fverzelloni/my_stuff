3f8794c0ecb5c2a750401a49d00006cf717beacf
fverzelloni <fverzell@cscs.ch>
2014-12-11 14:19:25 +0100

    added cases

--- generate_down_node_list.sh
+++ generate_down_node_list.sh
@@ -2,8 +2,44 @@
 
 pathtowith=5
 
-xtprocadmin |grep down | tr "," "\n" | awk '{ print $1" "$5" "$3 }' > list1
+function msg() { echo -e >&2 "$@"; }
 
-cat list1 | awk '{ print $1 }' | while read line ; do printf "nid%0*d\n" $pathtowith $line ; done > list3
+case "$1" in 
 
-paste list3 list1 | column -s $'\t' -t | awk '{ print $1","" "","" "","" "","" "","" ""," $3" "","$4 }'
+	"-f" | "--file" )
+			if [ "$2" != "" ]
+			then
+			xtprocadmin |grep down | tr "," "\n" | awk '{ print $1" "$5" "$3 }' > list1
+			cat list1 | awk '{ print $1 }' | while read line ; do printf "nid%0*d\n" $pathtowith $line ; done > list3
+			paste list3 list1 | column -s $'\t' -t | awk '{ print $1","" "","" "","" "","" "","" ""," $3" "","$4 }' > $2
+			rm list1 list3
+			else
+			echo "usage: ${0##*/} -f|--file filename {to create a file} "
+                        echo "usage: ${0##*/} -o|--output {to print in stdout}"
+                        exit 1
+			fi
+			;;
+
+	"-o" | "--output" )
+			xtprocadmin |grep down | tr "," "\n" | awk '{ print $1" "$5" "$3 }' > list1
+			cat list1 | awk '{ print $1 }' | while read line ; do printf "nid%0*d\n" $pathtowith $line ; done > list3
+			paste list3 list1 | column -s $'\t' -t | awk '{ print $1","" "","" "","" "","" "","" ""," $3" "","$4 }' 
+			rm list1 list3
+			;;
+
+	"-h" | "--help" )
+			echo "usage: ${0##*/} -f|--file filename {to create a file} "
+			echo "usage: ${0##*/} -o|--output {to print in stdout}"
+                        exit 1
+                        ;;
+
+	*)		
+			msg "usage: ${0##*/} -f|--file filename {to create a file}"
+			msg "usage: ${0##*/} -o|--output {to print in stdout}"
+			exit 1
+			;;
+
+
+esac
+
+exit 0
