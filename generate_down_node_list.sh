e8681d9303a5e2d62a69ad079aa9cd47f16a396c
fverzelloni <fverzell@cscs.ch>
2014-12-12 15:13:17 +0100

    new options

--- generate_down_node_list.sh
+++ generate_down_node_list.sh
@@ -1,52 +1,100 @@
 #!/bin/bash -l
 
-pathtowith=5
+#############################################################################################
+#  Generate down/admindown nodes list
+#
+#  Version 1.0 FV/12 December 2014
+#############################################################################################
+
+
+## VARs
+
+PATHTOWITH=5
+FILE_PATH=/tmp/nodes_down
+HOSTNAME=$(hostname | sed 's/[0-9]//g')
+DATE=$(date +%d%m%y_%H%M)
+CLOG=$(ls -tr1 $FILE_PATH/*.log | tail -n1)
+OLOG=$(ls -tr1 $FILE_PATH/*.log | tail -n2 | head -n1)
+DIFF=$(diff -a --suppress-common-lines -y $OLOG $CLOG )
+
+## ROUTINE
+
+if [ ! -d $FILE_PATH ]
+	then 
+		mkdir $FILE_PATH
+fi
 
 function msg() { echo -e >&2 "$@"; }
 
 case "$1" in 
+	
+	"-a" | "-auto" )
+			${0##*/} -o > $FILE_PATH/nodes_down_$DATE.log
+			;;
 
 	"-f" | "--file" )
 			if [ "$2" != "" ]
 			then
-			xtprocadmin |grep down | tr "," "\n" | awk '{ print $1" "$5" "$3 }' > list1
-			cat list1 | awk '{ print $1 }' | while read line ; do printf "nid%0*d\n" $pathtowith $line ; done > list3
-			paste list3 list1 | column -s $'\t' -t | awk '{ print $1","" "","" "","" "","" "","" ""," $3" "","$4 }' > $2
-			rm list1 list3
+				${0##*/} -o > $2
 			else
-			msg "usage: ${0##*/} -c|--count FILENAME {to count down/admindown nodes}"
-                        msg "usage: ${0##*/} -f|--file FILENAME {to create a file}"
-                        msg "usage: ${0##*/} -o|--output {to print in stdout}"
+				${0##*/} -h
                         exit 1
 			fi
 			;;
 
-	"-o" | "--output" )
-			xtprocadmin |grep down | tr "," "\n" | awk '{ print $1" "$5" "$3 }' > list1
-			cat list1 | awk '{ print $1 }' | while read line ; do printf "nid%0*d\n" $pathtowith $line ; done > list3
-			paste list3 list1 | column -s $'\t' -t | awk '{ print $1","" "","" "","" "","" "","" ""," $3" "","$4 }' 
-			rm list1 list3
-			;;
-
-	"-c" | "--count" )
-                        xtprocadmin |grep down | tr "," "\n" | awk '{ print $1" "$5" "$3 }' > list1
-                        cat list1 | awk '{ print $1 }' | while read line ; do printf "nid%0*d\n" $pathtowith $line ; done > list3
-                        paste list3 list1 | column -s $'\t' -t | awk '{ print $1","" "","" "","" "","" "","" ""," $3" "","$4 }' | wc -l
-                        rm list1 list3
+	"-e" | "--email" )
+			if [ "$2" != "" ]
+			then
+				${0##*/} -o | mail -s "Nodes down list $HOSTNAME" $2
+			else
+                                ${0##*/} -h
+                        exit 1
+                        fi
                         ;;
 
+	"--csv" )
+			if [ "$2" != "" ]
+                        then
+				xtprocadmin |grep down | tr "," "\n" | awk '{ print $1" "$5" "$3 }' > $FILE_PATH/list1
+				cat $FILE_PATH/list1 | awk '{ print $1 }' | while read line ; do printf "nid%0*d\n" $PATHTOWITH $line ; done > $FILE_PATH/list2
+				paste $FILE_PATH/list2 $FILE_PATH/list1 | column -s $'\t' -t | awk '{ print  $1","" "","" "","" "","" "","" ""," $3" "","$4 }' > $2
+				rm $FILE_PATH/list1 $FILE_PATH/list2
+			else
+                                ${0##*/} -h
+                        exit 1
+                        fi
+			;;
+
+	"-o" | "--output" )
+			xtprocadmin |grep down | tr "," "\n" | awk '{ print $1" "$5" "$3 }' > $FILE_PATH/list1
+                        cat $FILE_PATH/list1 | awk '{ print $1 }' | while read line ; do printf "nid%0*d\n" $PATHTOWITH $line ; done > $FILE_PATH/list2
+                        paste $FILE_PATH/list2 $FILE_PATH/list1 | column -s $'\t' -t | awk '{ printf ("%-11s%-12s%-s\n" ,$1, $3, $4) }'
+                        rm $FILE_PATH/list1 $FILE_PATH/list2
+                        ;;
+
+	"-c" | "--count" )
+			${0##*/} -o | wc -l
+                        ;;
+
+	"-C" | "--compare" )
+			if [ -n "$DIFF" ]
+			        then
+                			echo $DIFF | sed 's/,/ /g' 
+			fi
+			;;
+
 	"-h" | "--help" )
-			msg "usage: ${0##*/} -c|--count {to count down/admindown nodes} "
-			msg "usage: ${0##*/} -f|--file FILENAME {to create a file} "
-			msg "usage: ${0##*/} -o|--output {to print in stdout}"
+			${0##*/}
                         exit 1
                         ;;
 
 	* )		
-			msg "usage: ${0##*/} -c|--count FILENAME {to count down/admindown nodes}"
-			msg "usage: ${0##*/} -f|--file FILENAME {to create a file}"
-			msg "usage: ${0##*/} -o|--output {to print in stdout}"
-			exit 1
+			msg "usage: ${0##*/} -c|--count {to count down/admindown nodes} "
+                        msg "usage: ${0##*/} --csv FILENAME {to genereate an output file in CSV format} "
+                        msg "usage: ${0##*/} -e|--email email@address {to send an email with the attached down nodes list} "
+                        msg "usage: ${0##*/} -f|--file FILENAME {to create a file} "
+                        msg "usage: ${0##*/} -o|--output {to print in stdout}"
+                        exit 1
 			;;
 
 
