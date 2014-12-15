2be2e27c59734b7502d520bfbf58788f782d4054
fverzelloni <fverzell@cscs.ch>
2014-12-15 11:26:33 +0100

    second commit

--- compare_down.sh
+++ compare_down.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-COMMAND=`/apps/daint/system/scripts/generate_down_node_list.sh -C`
+
+# VAR's
+HOSTNAME="$(hostname | sed 's/[0-9]//g')"
+COMMAND=`/apps/$HOSTNAME/system/scripts/generate_down_node_list.sh -C`
+RECIPIENT=email@email.ch
 
 if [ ! -z "$COMMAND" ]
 	then
-		echo "$COMMAND" | sed 's/,/ /g' | mail -s "Nodes down update"  fverzell@cscs.ch 
+		echo "$COMMAND" | sed 's/,/ /g' | mail -s "Nodes down update"  $RECIPIENT
 fi
