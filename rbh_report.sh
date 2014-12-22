ac350467e07539c58eacfbbedea788e98454d475
fverzelloni <fverzell@cscs.ch>
2014-12-22 14:49:50 +0100

    second commit

--- rbh_report.sh
+++ rbh_report.sh
@@ -15,22 +15,22 @@ function msg() { echo -e >&2 "$@"; }
 case "$1" in
 
 	"-i" )
-		RBH_INFO=$(ssh rbhdaint@daintrbh01 "/usr/sbin/rbh-report -f /etc/robinhood.d/tmpfs/rbh_query.conf -i 2>/dev/null ")
+		RBH_INFO=$(ssh user@server "/usr/sbin/rbh-report -f /etc/robinhood.d/tmpfs/rbh_query.conf -i 2>/dev/null ")
                 echo -e "$RBH_INFO"
                 ;;
 
 	"-tu" )
-		RBH_TOPUSERS=$(ssh rbhdaint@daintrbh01 "/usr/sbin/rbh-report -f /etc/robinhood.d/tmpfs/rbh_query.conf --top-users 2>/dev/null | tail -n+1 |sed 's/,//'g")
+		RBH_TOPUSERS=$(ssh user@server "/usr/sbin/rbh-report -f /etc/robinhood.d/tmpfs/rbh_query.conf --top-users 2>/dev/null | tail -n+1 |sed 's/,//'g")
 		echo -e "$RBH_TOPUSERS"
 		;;
 	
 	"-g" )
-		RBH_GROUP=$(ssh rbhdaint@daintrbh01 "/usr/sbin/rbh-report -f /etc/robinhood.d/tmpfs/rbh_query.conf -g $2 2>/dev/null ")
+		RBH_GROUP=$(ssh user@server "/usr/sbin/rbh-report -f /etc/robinhood.d/tmpfs/rbh_query.conf -g $2 2>/dev/null ")
 		echo -e "$RBH_GROUP "
 		;;
 
 	"-u" )
-		RBH_USER=$(ssh rbhdaint@daintrbh01 "/usr/sbin/rbh-report -f /etc/robinhood.d/tmpfs/rbh_query.conf -u $2 2>/dev/null | sed 's/,//'g")
+		RBH_USER=$(ssh user@server "/usr/sbin/rbh-report -f /etc/robinhood.d/tmpfs/rbh_query.conf -u $2 2>/dev/null | sed 's/,//'g")
 		echo -e "$RBH_COMMAND"
 		;;
 	
