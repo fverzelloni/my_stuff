71b9d2884d7a949b55fefe6ebbe476d34651d636
fverzelloni <fverzell@cscs.ch>
2015-01-22 17:23:35 +0100

    second commit

--- osc_tuning_maker.sh
+++ osc_tuning_maker.sh
@@ -4,8 +4,12 @@ FILE=/tmp/osc
 RPCS=64
 DIRTY=256
 # Get the OST path
-#  pdsh -w nid000[32-43] ls /proc/fs/lustre/osc/ | grep snx | sed 's/://g'
-#  pdsh -w nid000[32-43] ls /proc/fs/lustre/osc/ | grep sant | sed 's/://g'
+# xtprocadmin | grep comp | awk '{ print $1 }' 
+# pdsh -w nid000[32-43] ls /proc/fs/lustre/osc/ | grep snx | sed 's/://g'
+# pdsh -w nid000[32-43] ls /proc/fs/lustre/osc/ | grep sant | sed 's/://g'
 
 cat /tmp/osc | while  read -r f1 f2 ; do echo " ssh $f1 \"echo $DIRTY > /proc/fs/lustre/osc/$f2/max_dirty_mb\" "; done 
 cat /tmp/osc | while  read -r f1 f2 ; do echo " ssh $f1 \"echo $RPCS  > /proc/fs/lustre/osc/$f2/max_rpcs_in_flight\" "; done
+
+cat /tmp/osc | while  read -r f1 f2 ; do echo " ssh $f1 cat /proc/fs/lustre/osc/$f2/max_dirty_mb\" "; done
+cat /tmp/osc | while  read -r f1 f2 ; do echo " ssh $f1 cat /proc/fs/lustre/osc/$f2/max_rpcs_in_flight\" "; done
