ad59cd111ea8bd6a1334bc336056065903c3d7ba
fverzelloni <fverzell@cscs.ch>
2015-04-09 14:18:04 +0200

    new fix

--- ssh_fix.sh
+++ ssh_fix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 
 # Data collection
-PDSH=/opt/cray/pdsh/2.26-1.0502.51215.1.1.ari/bin/pdsh
-XTPROCADMIN=/opt/cray/sdb/1.0-1.0502.55976.5.27.ari/bin/xtprocadmin
+PDSH=/opt/cray/pdsh/default/bin/pdsh
+XTPROCADMIN=/opt/cray/sdb/default/bin/xtprocadmin
 UPNODES_LIST=$( $XTPROCADMIN | grep compute | grep up | awk '{ print $3 }' | tr "\n" "," )
 UPNODES_NUMB=$( $XTPROCADMIN | grep compute | grep -c up)
 NODESOUT=$($PDSH -u 2 -w $UPNODES_LIST date 2>&1 )
@@ -14,8 +14,10 @@ SSHERR=$(echo "$NODESOUT" | grep "Connection refused" | wc -l )
 if [ ! -z "$SSHERRNODE" ]
 	then
 		echo $SSHERRNODE $(date +%d%m%y) >> /var/log/ssh_fail.log 
-		#for i in echo $SSHERRNODE ; do  ssh root@dora01 "hostname && /opt/cray/nodehealth/5.1-1.0502.56494.9.2.ari/bin/pcmd -t 5 -r -n $i '/etc/init.d/sshd start'" ; done
-		for i in echo $SSHERRNODE ; do  ssh root@dora01 "hostname && /opt/cray/nodehealth/5.1-1.0502.56494.9.2.ari/bin/pcmd -t 5 -r -n $i '/etc/init.d/sshd.logging start'" ; done
+		for i in $SSHERRNODE
+                  do
+                  ssh root@login1 "/opt/cray/nodehealth/default/bin/pcmd -t 5 -r -n $i '/etc/init.d/sshd start'"
+                   done
 	else
 		echo "No nodes to fix :-)"
 fi
