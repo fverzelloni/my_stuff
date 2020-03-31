28a29e16f932c241c1f7b3645d73c8ed687b2d5e
fverzelloni <fabio.verzelloni@gmail.com>
2020-03-31 10:55:16 +0200

    Update sysinfo.py

--- sysinfo.py
+++ sysinfo.py
@@ -1,10 +1,11 @@
-#!/usr/local/bin/confluence/env/bin/python3
+#!/usr/bin/python3
 
 import platform
 import sys
 import os
 import fileinput
 import subprocess
+import socket
 
 #Variables
 http_srv = 'someo.cscs.ch'
@@ -12,6 +13,9 @@ hostname = platform.node()
 
 # Save output in /tmp/sys_info_HOSTNAME.html
 sys.stdout = open('/tmp/sys_info_'+hostname.split(".")[0]+'.html','w')
+
+# Get IP
+host_ip = socket.gethostbyname(hostname)
 
 # Architecture
 arch = platform.architecture()[0]
@@ -22,12 +26,11 @@ distro = str(platform.linux_distribution())
 # Kernel Version
 kernel = str(platform.release())
 
-# distribution
+# Distribution
 dist = platform.dist()
 dist = " ".join(x for x in dist)
 
-# processor
-#print("Processors: ")
+# Processor
 with open("/proc/cpuinfo", "r")  as f:
     info = f.readlines()
 cpuinfo = [x.strip().split(":")[1] for x in info if "model name"  in x]
@@ -36,49 +39,60 @@ cpuinfo = [x.strip().split(":")[1] for x in info if "model name"  in x]
 #print("Memory Info: ")
 with open("/proc/meminfo", "r") as f:
     lines = f.readlines()
+    memtotal = lines[0].split()
+    memfree = lines[1].split()
+    memtotal = ( int(memtotal[1]) / 1024 / 1024 )
+    memfree = ( int(memfree[1]) / 1024 / 1024 )
 
-#print("     " + lines[0].strip())
-#print("     " + lines[1].strip())
-
-# uptime
+# Uptime
 uptime = None
 with open("/proc/uptime", "r") as f:
     uptime = f.read().split(" ")[0].strip()
 uptime = int(float(uptime))
 uptime_hours = uptime // 3600
 uptime_minutes = (uptime % 3600) // 60
-#print("Uptime: " + str(uptime_hours) + ":" + str(uptime_minutes) + " hours")
 
 # Load
 with open("/proc/loadavg", "r") as f:
     load = f.read().strip()
 #    print("Average Load: " + f.read().strip())
+
+# Create HTML
 print("<html>")
 print("<br>")
+print("<b>"+ hostname + "</b>")
+print("<br>")
 print("Hostname: " + hostname)
+print("<br>")
+print("Ip Address: " + host_ip)
 print("<br>")
 print("Architecture: " + arch)
 print("<br>")
 print("Distribution: " + dist)
 print("<br>")
 print("Kernel: " + kernel)
+#print("<br>")
+#BCM Version
+#bcmver = subprocess.getoutput('cmsh -c "main versioninfo"')
+if os.path.isfile('/cm/local/apps/cmd/bin/cmsh'):
+    bcmver = subprocess.getoutput('cmsh -c "main versioninfo"')
+    bcmver_split = (bcmver.splitlines())
+    for x in bcmver_split:
+        print("<br>" + x )
 print("<br>")
+#Uptime
 print("Uptime: " + str(uptime_hours) + ":" + str(uptime_minutes) + " hours")
 print("<br>")
-print("Processors: ")
-for index, item in enumerate(cpuinfo):
-    print("    " + str(index) + ": " + item)
-#print("    " + str(index) + ": " + item))
-print("<br>")
+#Load
 print("Average Load: " + load)
 print("<br>")
-print("Memory Info: ")
-print("     " + lines[0].strip())
-print("     " + lines[1].strip())
+#Memory
+print("Memory Total: " + str(int(memtotal)) + ' GB')
+print("<br>")
+print("Memory Free: " + str(int(memfree)) + ' GB')
+print("<br>")
 print("</html>")
 sys.stdout.close()
-
-#os.rename('/tmp/sys_info.html','/tmp/sys_info_'+hostname.split(".")[0]+'.html')
 
 # Copy HTML to remote server
 p = subprocess.Popen(['scp', '/tmp/sys_info_'+hostname.split(".")[0]+'.html', 'root@'+ http_srv +':/var/www/html/confluence/sys_info_'+hostname.split(".")[0]+'.html'])
@@ -86,3 +100,9 @@ sts = p.wait()
 
 # Delete files from /tmp
 os.remove('/tmp/sys_info_'+hostname.split(".")[0]+'.html')
+
+#print("Processors: ")
+#for index, item in enumerate(cpuinfo):
+#    print("    " + str(index) + ": " + item)
+#print("    " + str(index) + ": " + item))
+#print("<br>")
