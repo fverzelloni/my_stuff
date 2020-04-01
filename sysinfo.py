a0602bf7068ecd08d3b9cae7bb7dfb51f72ecf20
fverzelloni <fabio.verzelloni@gmail.com>
2020-04-01 13:37:23 +0200

    Update sysinfo.py

--- sysinfo.py
+++ sysinfo.py
@@ -6,14 +6,14 @@ import os
 import fileinput
 import subprocess
 import socket
+import platform
 from subprocess import Popen, PIPE
-#from shutil import which
 
 #Variables
 http_srv = 'someo.cscs.ch'
 hostname = platform.node()
 
-#Functions
+##Functions
 def check_program_exists(name):
     p = Popen(['/usr/bin/which', name], stdout=PIPE, stderr=PIPE)
     p.communicate()
@@ -24,34 +24,24 @@ sys.stdout = open('/tmp/sys_info_'+hostname.split(".")[0]+'.html','w')
 
 # Get IP
 host_ip = socket.gethostbyname(hostname)
-
-# Architecture
-arch = platform.architecture()[0]
-
 # Linux Distribution
 distro = str(platform.linux_distribution())
-
 # Kernel Version
 kernel = str(platform.release())
-
 # Distribution
 dist = platform.dist()
 dist = " ".join(x for x in dist)
-
 # Processor
 with open("/proc/cpuinfo", "r")  as f:
     info = f.readlines()
 cpuinfo = [x.strip().split(":")[1] for x in info if "model name"  in x]
-
 # Memory
-#print("Memory Info: ")
 with open("/proc/meminfo", "r") as f:
     lines = f.readlines()
     memtotal = lines[0].split()
     memfree = lines[1].split()
     memtotal = ( int(memtotal[1]) / 1024 / 1024 )
     memfree = ( int(memfree[1]) / 1024 / 1024 )
-
 # Uptime
 uptime = None
 with open("/proc/uptime", "r") as f:
@@ -59,39 +49,39 @@ with open("/proc/uptime", "r") as f:
 uptime = int(float(uptime))
 uptime_hours = uptime // 3600
 uptime_minutes = (uptime % 3600) // 60
-
 # Load
 with open("/proc/loadavg", "r") as f:
     load = f.read().strip()
 
+##
 # Create HTML
 print("<html>")
-print("<br>")
 print("<b>"+ hostname + "</b>")
-print("<br>")
-print("Hostname: " + hostname)
-print("<br>")
-print("Ip Address: " + host_ip)
-print("<br>")
-print("Architecture: " + arch)
-print("<br>")
-print("Distribution: " + dist)
-print("<br>")
-print("Kernel: " + kernel)
-print("<br>")
+print("<br> Hostname: " + hostname )
+print("<br> Ip Address: " + host_ip )
+#Processors
+if check_program_exists('lscpu') == True:
+    cpuinfo = subprocess.getoutput('lscpu')
+    cpuinfo_split = (cpuinfo.splitlines())
+    values = ["Architecture:", "CPU(s):", "Model name:", "Core(s) per socket:", "Thread(s) per core:" ]
+    for x in cpuinfo_split:
+        for y in values:
+            if y in x:
+                print("<br>" + x)
+print("<br> Distribution: " + dist )
+print("<br> Kernel: " + kernel )
+#print("<br>" + platform.processor() )
 #BCM Version
 if check_program_exists('cmsh') == True:
     bcmver = subprocess.getoutput('cmsh -c "main versioninfo"')
     bcmver_split = (bcmver.splitlines())
-    #print(type(bcmver_split))
     for x in bcmver_split:
         if "Manager" in x:
-            print("BCM Version: " + x.split()[2] + "<br>" )
+            print("<br> BCM Version: " + x.split()[2] )
 #Slurm Version
 if check_program_exists('sinfo') == True:
     slurmver = subprocess.getoutput('sinfo -V')
-    print("<br>")
-    print("Slurm Version: " + slurmver.split()[1] )
+    print("<br> Slurm Version: " + slurmver.split()[1] )
     print("<br>")
 #GPFS Version
 if check_program_exists('mmdiag') == True:
@@ -100,18 +90,13 @@ if check_program_exists('mmdiag') == True:
     for x in gpfsver_split:
         if "Current" in x:
             print(x)
-            print("<br>")
 #Uptime
-print("Uptime: " + str(uptime_hours) + ":" + str(uptime_minutes) + " hours")
-print("<br>")
+print("<br> Uptime: " + str(uptime_hours) + "h" ":" + str(uptime_minutes) + "m")
 #Load
-print("Average Load: " + load)
-print("<br>")
+print("<br> Average Load: " + load )
 #Memory
-print("Memory Total: " + str(int(memtotal)) + ' GB')
-print("<br>")
-print("Memory Free: " + str(int(memfree)) + ' GB')
-print("<br>")
+print("<br> Memory Total: " + str(int(memtotal)) + ' GB')
+print("<br> Memory Free: " + str(int(memfree)) + ' GB')
 print("</html>")
 sys.stdout.close()
 
